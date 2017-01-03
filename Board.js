c9a33c3e9ee781a67308d96e4090196a9ea012da
Dagmara Jagodzinska <dag.jagodzinska@wp.pl>
2017-01-03 13:21:30 +0100

    first

---
+++ Board.js
@@ -0,0 +1,31 @@
+var board = {
+	name: 'Tablica Kanban',
+	createColumn: function(column) {
+	  this.element.append(column.element);
+	  initSortable();
+	},
+	element: $('#board .column-container')
+};
+
+$('.create-column')
+	.click(function(){
+		board.createColumn(new Column(prompt('Wpisz nazwę kolumny')));
+	});
+	
+function initSortable() {
+	$('.create-column')
+	    .click(function() {
+	        var columnName = prompt('Wpisz nazwę kolumny');
+	        $.ajax({
+	    		url: baseUrl + '/column',
+	    		method: 'POST',
+	    		data: {
+	            	name: columnName
+	    		},
+	    		success: function(response){
+	    			var column = new Column(response.id, columnName);
+	    			board.createColumn(column);
+	          	}
+	        });
+	});
+  }
